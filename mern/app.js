169f446f18c84f10ba286ff794d84a37388bc8d4
ihorhub <ihor_babiy@ukr.net>
2021-04-16 01:46:28 +0300

    add  mailer

--- mern/app.js
+++ mern/app.js
@@ -1,25 +1,44 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const dotenv= require('dotenv')
 const path = require('path')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
-const apiRouter = require('./router/apiRouter')
-const app = express()
 dotenv.config()
+// dotenvConfigOutput.config();
+
+const { MONGO_URL, PORT } = require('./configs/config')
+const apiRouter = require('./router/api.router')
+
+const app = express()
+
 _connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.all('/', apiRouter)
+app.use('/', apiRouter)
 
-app.listen(5000, () => {
-  console.log('app listen 5000')
+// eslint-disable-next-line no-unused-vars
+app.use('*', (err, req, res, next) => {
+  res.status(err.status || 500).json({
+    customCode: err.customCode || 0,
+    message: err.message || '',
+  })
 })
-function_connectDB(){
-  mongoose.connect('mongoDB://localhost:27017',{useNewUrlParser:true, useUnifieldTopology:true})
-  const connection =mongoose.connection
-  connection.on('error',(error)=>{
+
+app.listen(PORT, () => {
+  console.log(`App listen ${PORT}`)
+})
+
+function _connectDB() {
+  mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  const { connection } = mongoose
+
+  connection.on('error', (error) => {
     console.log(error)
   })
 }
